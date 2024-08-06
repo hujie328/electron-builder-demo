42a22d3e0381f20aa29d9915f2a89d1499bb1d8d
胡杰 <hujie_0328@163.com>
2024-08-06 20:13:45 +0800

    应用打开浏览器

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-const { app, BrowserWindow, session } = require('electron')
+const { app, BrowserWindow, shell } = require('electron')
 const { logger } = require('./log/index.js')
 const path = require('path')
 const { registerIpcMainHandle } = require('./ipcModule/ipcMainFns.js')
@@ -17,8 +17,17 @@ try {
             }
         })
         // win.fullScreen = true // 开启全屏
-        win.loadFile(`${app.getAppPath()}/h5/index.html`)
+        if (params) {
+            win.loadURL(params)
+        } else {
+            win.loadFile(`${app.getAppPath()}/h5/index.html`)
+        }
         win.webContents.openDevTools()
+        win.webContents.setWindowOpenHandler((details) => {
+            let url = details.url
+            shell.openExternal(url);
+            return { action: 'deny' }
+        })
         win.webContents.on('did-finish-load', () => {
             startStaticServer(win)
         })
@@ -43,7 +52,7 @@ try {
     app.on('before-quit', () => {
         logger.info('触发事件：before-quit')
     })
-    app.on('触发事件：browser-window-created', (event, win) => {
+    app.on('browser-window-created', (event, win) => {
         win.maximize()
         win.webContents.on('did-finish-load', () => {
             logger.info('触发事件：before-quit')
@@ -63,55 +72,55 @@ try {
         // 如果不是默认协议客户端，则设置为默认协议客户端
         app.setAsDefaultProtocolClient('zgbigdata')
     }
+    // 此方法的返回值表示你的应用程序实例是否成功取得了锁。
+    // 如果它取得锁失败，你可以假设另一个应用实例已经取得了锁并且仍旧在运行，并立即退出。
+    const gotTheLock = app.requestSingleInstanceLock();
+    if (!gotTheLock) {
+        // 如果获取失败，说明已经有实例在运行了，直接退出
+        app.quit();
+    } else {
+        // 相当于 MacOS上的 open-url 事件
+        app.on('second-instance', (event, commandLine, workingDirectory) => {
+            // 用户正在尝试运行第二个实例，我们需要让焦点指向我们的窗口
+            logger.info('通过浏览器协议打开--------')
+            logger.info(commandLine)
+            logger.info('通过浏览器协议打开--------')
+            let params = commandLine.pop()?.split('zgbigdata://')[1]
+            logger.info(`协议参数：${params}`)
+            let mainWindow = BrowserWindow.getAllWindows()[0];
+
+            if (mainWindow) {
+                if (mainWindow.isMinimized()) mainWindow.restore();
+                mainWindow.focus();
+                if (params) {
+                    mainWindow.loadURL(params);
+                }
+            } else {
+                createWindow(params)
+            }
+        })
+
+    }
+
+    const isHandleAfterReady = () => {
+        // windows如果是通过url schema启动则发出时间处理
+        // 启动参数超过1个才可能是通过url schema启动
+        if (process.argv.length > 1) {
+            if (!app.isReady()) {
+                app.once("browser-window-created", () => {
+                    // app 未打开时，通过 open-url打开 app，此时可能还没 ready，需要延迟发送事件
+                    // 此段ready延迟无法触发 service/app/ open-url 处理，因为saga初始化需要时间
+                    app.emit("second-instance", null, process.argv);
+                });
+            } else {
+                app.emit("second-instance", null, process.argv);
+            }
+        } else {
+            createWindow()
+        }
+    };
 } catch (e) {
     logger.error(e)
     process.exit();
 }
 
-// 此方法的返回值表示你的应用程序实例是否成功取得了锁。
-// 如果它取得锁失败，你可以假设另一个应用实例已经取得了锁并且仍旧在运行，并立即退出。
-const gotTheLock = app.requestSingleInstanceLock();
-if (!gotTheLock) {
-    // 如果获取失败，说明已经有实例在运行了，直接退出
-    app.quit();
-} else {
-    // 相当于 MacOS上的open-url 事件
-    app.on('second-instance', (event, commandLine, workingDirectory) => {
-        // 用户正在尝试运行第二个实例，我们需要让焦点指向我们的窗口
-        logger.info('通过浏览器协议打开--------')
-        logger.info(commandLine)
-        logger.info('通过浏览器协议打开--------')
-        let params = commandLine.pop()?.split('zgbigdata://')[1]
-        logger.info(`协议参数：${params}`)
-        let mainWindow = BrowserWindow.getAllWindows()[0];
-
-        if (mainWindow) {
-            if (mainWindow.isMinimized()) mainWindow.restore();
-            mainWindow.focus();
-            if (params) {
-                mainWindow.loadURL(params);
-            }
-        } else {
-            createWindow(params)
-        }
-    })
-
-}
-
-const isHandleAfterReady = () => {
-    // windows如果是通过url schema启动则发出时间处理
-    // 启动参数超过1个才可能是通过url schema启动
-    if (process.argv.length > 1) {
-        if (!app.isReady()) {
-            app.once("browser-window-created", () => {
-                // app 未打开时，通过 open-url打开 app，此时可能还没 ready，需要延迟发送事件
-                // 此段ready延迟无法触发 service/app/ open-url 处理，因为saga初始化需要时间
-                app.emit("second-instance", null, process.argv);
-            });
-        } else {
-            app.emit("second-instance", null, process.argv);
-        }
-    } else {
-        createWindow()
-    }
-};
